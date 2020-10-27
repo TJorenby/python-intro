52b721037ca9dca7e049fe529e42ace71c2b6e9d
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-10-27 12:13:03 -0500

    program created using variables

--- python-tutorial.py
+++ python-tutorial.py
@@ -1,3 +1,16 @@
-print("Hello World")
+# print("Hello World")
 
-print("Tyler Edwin Richard Jorenby")
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# create a variable
+
+character_name = "Tyler"
+character_age = "35"
+print("My name is " + character_name + ", and")
+print("I'm " + character_age + " years old")
+print("I like the name " + character_name + " ,")
+print("and someday, I will be " + character_age + " years old.")
